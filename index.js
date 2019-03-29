3f4cea6b4eb830c3cfe38537489eb0a06f8e4bb8
Axel Erlich <axeluck1@gmail.com>
2019-03-28 23:52:00 -0300

    Prueba de redeploy

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -12,8 +13,10 @@ app.set('views', path.join(__dirname, 'views'));
 // Configuro el view engine
 app.set('view engine', 'ejs')
 
-// Hago un get
+// Hago el get por defecto
 app.get('/', (req, res) => res.render('pages/index'))
+// Get de cool face
+app.get('/cool', (req, res) => res.send(cool()))
 
 // Lanzo el servidor
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
