3e58a6c6e3e30391196badecedb69c58b6792d41
Axel Erlich <axel.erlich@yahoo.com.ar>
2019-05-10 10:22:22 -0300

    Configuro node para que levante angular

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -6,17 +5,8 @@ const PORT = process.env.PORT || 5000
 var app = express();
 
 // Archivos estaticos
-app.use(express.static(path.join(__dirname, 'public')))
-// Vistas
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'Client/dist/Client')))
 
-// Configuro el view engine
-app.set('view engine', 'ejs')
-
-// Hago el get por defecto
-app.get('/', (req, res) => res.render('pages/index'))
-// Get de cool face
-app.get('/cool', (req, res) => res.send(cool()))
 
 // Lanzo el servidor
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
