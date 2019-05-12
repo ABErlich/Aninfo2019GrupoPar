0f633c37cfe7356a93a99dbbfa481e0d818c6daa
Axel Erlich <axeluck1@gmail.com>
2019-05-12 17:02:51 -0300

    Agrego una base de datos postgre

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg');
 const PORT = process.env.PORT || 5000
 
+// Inicializo la coneccion con la BDD
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+  });
 var app = express();
 
 // Archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+// Endpoint para probar la BDD
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+
+
+
+  // Cualquier otro request que pida y no sea parte de mi api
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
+
 // Lanzo el servidor
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
