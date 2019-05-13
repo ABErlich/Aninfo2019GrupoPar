c3f7b390f37a1bea7eefd668de0f66c5f945714b
mpicco <martinalejandropicco1996@gmail.com>
2019-05-12 23:20:25 -0300

    Add 'See Projects' with created projects

--- features/projects/see_projects.feature
+++ features/projects/see_projects.feature
@@ -5,3 +5,8 @@ Feature: See projects
         Given I am in the projects page
         And no projects are created
         Then I should see an empty list of projects
+
+    Scenario: Projects created
+        Given I am in the projects page
+        And there is a project created with name 'PROJECT'
+        Then I should see a list with one project named 'PROJECT'
--- features/projects/step_definitions/see_projects_steps.js
+++ features/projects/step_definitions/see_projects_steps.js
@@ -4,7 +4,37 @@ const { Given, When, Then } = require('cucumber');
 Given('no projects are created', async function () {
 });
 
+Given('there is a project created with name {string}', async function (name) {
+    await this.page.click("#botonito");
+
+    await this.page.focus('#new-project-id');
+    await this.page.keyboard.type('testnewid');
+
+    await this.page.focus('#new-project-leader');
+    await this.page.keyboard.type('testnewleader');
+
+    await this.page.focus('#new-project-description');
+    await this.page.keyboard.type('testnewdescription');
+
+    await this.page.focus('#new-project-name');
+    await this.page.keyboard.type(name);
+
+    await this.page.click("#new-project-submit");
+
+    await this.page.click('#nav-router-proyectos');
+});
+
 Then('I should see an empty list of projects', async function () {
     const itemCount = await this.page.$$eval('.project-list-item', (elements) => elements.length || 0);
     assert.equal(itemCount, 0);
 })
+
+Then('I should see a list with one project named {string}', async function (name) {
+    // const items = await this.page.$$eval('.project-list-item .project-title', 
+    //                                     (elements) => (elements || []).map(e => e.innerText));
+    // assert.equal(items.length, 1);
+    // assert.equal(items[0], name);
+    const items = await this.page.$$eval('.project-list-item', (elements) => elements.length || 0);
+    assert.equal(items, 1);
+})
+
