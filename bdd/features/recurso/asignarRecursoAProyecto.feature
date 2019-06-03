5d5db38eef36debf8a1750f46b983ba931a82275
Santiago <czuczinsky@hotmail.com>
2019-06-03 00:39:52 -0300

    asignar a dos proyectos

--- bdd/features/recurso/asignarRecursoAProyecto.feature
+++ bdd/features/recurso/asignarRecursoAProyecto.feature
@@ -5,15 +5,24 @@ Feature: Agregar recurso a proyecto
     Scenario: Agregar recurso a proyecto correctamente
         Given tengo un recurso
         And tengo un proyectoo
-        When asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '01/12/2019'
+        When asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '01/12/2019', disponibilidad 80
         Then al consultar su rol vale 'programador'
         And al consultar su fecha de inicio vale '01/01/2019'
         And al consultar su fecha de fin vale '01/12/2019'
+        And al consultar su disponibilidad vale '80'
 
     Scenario: Agregar recurso a dos proyectos en el mismo periodo
         Given tengo un recurso
         And tengo dos proyectos
-        When asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '01/12/2019', al proyecto 'P1'
-        And asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '01/12/2019', al proyecto 'P2'
-        Then el proyecto 'P1' tiene un recurso
-        And el proyecto 'P2' tiene un recurso
+        When asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '01/12/2019', al proyecto p1, disponibilidad 40
+        And asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '01/12/2019', al proyecto p2, disponibilidad 40
+        Then el recurso se encuentra asignado en 'p1'
+        And el recurso se encuentra asignado en 'p2'
+
+    Scenario: Agregar recurso a dos proyectos en el mismo periodo invalido
+        Given tengo un recurso
+        And tengo dos proyectos
+        When asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '08/01/2019', al proyecto p1, disponibilidad 40
+        And asigno a un recurso con rol 'programador', fecha de inicio '01/01/2019', fecha de fin '08/01/2019', al proyecto p3, disponibilidad 90
+        Then el recurso se encuentra asignado en 'p1'
+        And el recurso no se encuentra asignado en 'p3'
--- bdd/steps/recurso/asignarRecursoAProyecto.js
+++ bdd/steps/recurso/asignarRecursoAProyecto.js
@@ -11,8 +11,8 @@ Given("tengo un proyectoo", function () {
     this.proyecto = new Proyecto('proyectoTest', 'PYT', 'Esto es un proyecto test', 'El Lider', '');
 });
 
-When("asigno a un recurso con rol {string}, fecha de inicio {string}, fecha de fin {string}", function (rol, fechaInicio, fechaFin) {
-    this.recurso.asignarProyecto(this.proyecto, rol, fechaInicio, fechaFin);
+When("asigno a un recurso con rol {string}, fecha de inicio {string}, fecha de fin {string}, disponibilidad {int}", function (rol, fechaInicio, fechaFin, disponibilidad) {
+    this.recurso.asignarProyecto(this.proyecto, rol, fechaInicio, fechaFin, disponibilidad);
 });
 
 Then("al consultar su rol vale {string}", function (rol) {
@@ -27,3 +27,36 @@ Then("al consultar su fecha de fin vale {string}", function (fechaFin) {
     assert.equal(this.recurso.obtenerFechaFinEnProyecto(this.proyecto), fechaFin);
 });
 
+Then("al consultar su disponibilidad vale {string}", function (disponibilidad) {
+    assert.equal(this.recurso.obtenerDisponibilidadEnProyecto(this.proyecto), disponibilidad);
+});
+
+Given("tengo dos proyectos", function () {
+    this.proyecto1 = new Proyecto('p1', 'p1', 'Esto es un proyecto test', 'El Lider', '');
+    this.proyecto2 = new Proyecto('p2', 'p2', 'Esto es un proyecto test', 'El Lider', '');
+    this.proyecto3 = new Proyecto('p3', 'p3', 'Esto es un proyecto test', 'El Lider', '');
+});
+
+When("asigno a un recurso con rol {string}, fecha de inicio {string}, fecha de fin {string}, al proyecto p1, disponibilidad {int}", function (rol, fechaInicio, fechaFin, disponibilidad) {
+    this.recurso.asignarProyecto(this.proyecto1, rol, fechaInicio, fechaFin, disponibilidad);
+});
+
+When("asigno a un recurso con rol {string}, fecha de inicio {string}, fecha de fin {string}, al proyecto p2, disponibilidad {int}", function (rol, fechaInicio, fechaFin, disponibilidad) {
+    this.recurso.asignarProyecto(this.proyecto2, rol, fechaInicio, fechaFin, disponibilidad);
+});
+
+When("asigno a un recurso con rol {string}, fecha de inicio {string}, fecha de fin {string}, al proyecto p3, disponibilidad {int}", function (rol, fechaInicio, fechaFin, disponibilidad) {
+    try {
+        this.recurso.asignarProyecto(this.proyecto3, rol, fechaInicio, fechaFin, disponibilidad);
+    } catch (err) {
+        this.error = err.message;
+    }
+});
+
+Then("el recurso se encuentra asignado en {string}", function (codigoProyecto) {
+    assert.equal(this.recurso.estaEnProyecto(codigoProyecto), true);
+});
+
+Then("el recurso no se encuentra asignado en {string}", function (codigoProyecto) {
+    assert.equal(this.recurso.estaEnProyecto(codigoProyecto), false);
+});
