2ec7be44fd50445a99ab2aac3f7354566265d86b
mpicco <martinalejandropicco1996@gmail.com>
2019-06-02 23:05:58 -0300

    Features: umbral de riesgo de proyecto

--- model/Proyecto.js
+++ model/Proyecto.js
@@ -1,3 +1,5 @@
+const ConfiguracionRiesgo = require('./ConfiguracionRiesgo');
+
 module.exports = class Proyecto {
     constructor (nombre, codigo, descripcion, lider, requerimientos) {
         if (!nombre) { throw new Error('Parametro requerido: nombre'); }
@@ -14,6 +16,8 @@ module.exports = class Proyecto {
         this.tareas = [];
         this.horasPorRecurso = {};
         this.riesgos = [];
+
+        this.configuracionRiesgo = new ConfiguracionRiesgo(0, 0, 0, 1);
     }
 
     agregarTarea (tarea) {
@@ -37,5 +41,25 @@ module.exports = class Proyecto {
     
     agregarRiesgo (riesgo) {
         this.riesgos.push(riesgo);
+    }
+
+    asignarConfiguracionRiesgo (config) {
+        this.configuracionRiesgo = config;
+    }
+
+    enRiesgo () {
+        if (!this.riesgos.length) {
+            return false;
+        }
+
+        const umbral = this.configuracionRiesgo.obtenerUmbral();
+
+        for (const riesgo of this.riesgos) {
+            if (riesgo.exposicion() > umbral) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
